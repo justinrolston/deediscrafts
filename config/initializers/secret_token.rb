e6c268d62fcacb8013596b2448baab6d01d00794
Ricky Rickard, Jr <rerickardjr@gmail.com>
2013-06-06 19:27:33 -0400

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deediscrafts::Application.config.secret_key_base = 'a2504b610c4149468ac47f32412fc6552cd6337e0b12b082cc02967cccbdf2f28248c582864e6fc23a1134f01322c464aa03394477991e0c21293e36819cc871'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Deediscrafts::Application.config.session_store :cookie_store, key: '_deediscrafts_session'
