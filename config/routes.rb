77f2b04da1b744e1341b28e43e470ae990e17641
Ricky Rickard, Jr <rerickardjr@gmail.com>
2013-06-07 02:38:27 -0400

    working with images

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Deediscrafts::Application.routes.draw do
   get "home/index"
+  get "home/ricky/deediscrafts/images/130602_0000.jpg/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  root 'home/ricky/deediscrafts#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
